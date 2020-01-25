7f3107a00d1b926153c786a15faeeec2381f22a9
Tyler <mtylerrobbinsgmail.com>
2020-01-25 14:50:38 -0600

    updated12520

--- app_genre.sql
+++ app_genre.sql
@@ -1,14 +1,29 @@
-SELECT 
-DISTINCT(primary_genre) as gname,(
+SELECT (
 	CASE WHEN primary_genre ILIKE '%action%' OR primary_genre ILIKE '%adventure%' THEN 'Action/Adventure'
 	WHEN primary_genre ILIKE '%art%' OR primary_genre ILIKE '%design%' THEN 'Art & Design'
-	ELSE 'Other' END) AS genre_new
-FROM app_store_apps AS a
-UNION ALL
-SELECT DISTINCT(genres) as gname,(
-	CASE WHEN genres ILIKE '%action%' OR genres ILIKE '%adventure%' THEN 'Action/adventure'
-	WHEN genres ILIKE '%art%' OR genres ILIKE '%design%' THEN 'Art & Design'
-	ELSE 'Other' END) AS genre_new
+	WHEN primary_genre ILIKE '%education%' THEN 'Educational'
+	WHEN primary_genre ILIKE '%game%' OR primary_genre ILIKE '%puzzle%' THEN 'Games'
+	WHEN primary_genre ILIKE '%social%' THEN 'Social'
+	WHEN primary_genre ILIKE '%food%' THEN 'Food'
+	WHEN primary_genre ILIKE '%shop%' OR primary_genre ILIKE '%catalogs%' THEN 'Shopping'
+	WHEN primary_genre ILIKE '%entertain%' THEN 'Entertainment'
+	WHEN primary_genre ILIKE '%sport%' THEN 'Sports'
+	ELSE primary_genre END) AS genre_new
+FROM app_store_apps AS a 
+GROUP BY genre_new
+UNION
+SELECT (
+	CASE WHEN genres ILIKE '%art%' OR genres ILIKE '%design%' THEN 'Art & Design'
+	WHEN genres ILIKE '%education%' THEN 'Educational'
+	WHEN genres ILIKE '%trivia%' OR genres ILIKE '%game%' OR genres ILIKE '%puzzle%' OR genres ILIKE '%racing%' OR genres ILIKE '%action%' OR genres ILIKE '%adventure%' THEN 'Games'
+	WHEN genres ILIKE '%social%' THEN 'Social'
+	WHEN genres ILIKE '%music%' THEN 'Music'
+	WHEN genres ILIKE '%food%' THEN 'Food'
+	WHEN genres ILIKE '%shop%' OR genres ILIKE '%catalogs%' THEN 'Shopping'
+	WHEN genres ILIKE '%entertain%' THEN 'Entertainment'
+	WHEN genres ILIKE '%sport%' THEN 'Sports' 
+ 	ELSE 'other' END) AS genre_new
 FROM play_store_apps AS p 
-ORDER BY gname
+GROUP BY genre_new
+ORDER BY genre_new
 
--- app_trader_forest_round.sql
+++ app_trader_forest_round.sql
@@ -1,16 +1,22 @@
-SELECT DISTINCT(a.name),
-a.price as price,
-ROUND(FLOOR(a.rating*2)/2,1) as rating, 
+SELECT DISTINCT(name),
+price as price, 
+ROUND(FLOOR(
+	(CASE WHEN rating IS NULL THEN 0
+	ELSE rating END)
+	*2)/2,1) as rating, 
 primary_genre,
-a.content_rating
+content_rating
 FROM app_store_apps AS a
 UNION ALL
-SELECT DISTINCT(p.name),
-CAST(TRIM('$' FROM p.price) AS numeric),
-ROUND(FLOOR(p.rating*2)/2,1) as rating, 
+SELECT DISTINCT(name),
+CAST(TRIM('$' FROM price) AS numeric),
+ROUND(FLOOR((
+	CASE WHEN rating IS NULL THEN 0
+	ELSE rating END)*2)/2,1) as rating, 
 genres,
-p.content_rating 
+content_rating 
 FROM play_store_apps AS p
-	WHERE p.price IS NOT NULL 
-ORDER BY rating DESC
+	WHERE price IS NOT NULL 
+ORDER BY rating  DESC
+
 
