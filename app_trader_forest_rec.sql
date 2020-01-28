34e99c669c7daf036a2aee6d000d9ba4b3a187b0
Tyler <mtylerrobbinsgmail.com>
2020-01-28 17:37:14 -0600

    FINAL PUSH

--- app_trader_forest_rec.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT(p.name), p.price as price, p.rating, genres, primary_genre, a.content_rating
-FROM app_store_apps AS a
-FULL OUTER JOIN play_store_apps AS p
-	ON p.name = a.name 
-	WHERE p.price IS NOT NULL
